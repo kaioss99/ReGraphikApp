26c1985e8bf50e4df59b7e28baaa431ea4ee44b3
Luna <luna.beatrizalves@gmail.com>
2026-05-20 21:26:10 -0300

    Implemetação dos atributos da classe Usuário - Luna

--- ApiRestReGraphik/Models/Usuario.cs
+++ ApiRestReGraphik/Models/Usuario.cs
@@ -1,6 +1,28 @@
-﻿namespace ApiRestReGraphik.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ApiRestReGraphik.Models
 {
     public class Usuario
     {
+        [JsonPropertyName("id")]
+        public int ID { get; set; }
+
+        [JsonPropertyName("name")]
+        public string Nome { get; set; }
+
+        [JsonPropertyName("cpf")]
+        public string CPF { get; set; }
+
+        [JsonPropertyName("email")]
+        public string Email { get; set; }
+
+        [JsonPropertyName("login")]
+        public string Login { get; set; }
+
+        [JsonPropertyName("senha")]
+        public string Senha { get; set; }
+
+        [JsonPropertyName("data_cadastro")]
+        public DateTime DataCadastro { get; set; }
     }
 }
