6a4554b4900b9df153e49a73c92599248cacbfcf
Luna <luna.beatrizalves@gmail.com>
2026-05-20 21:32:42 -0300

    Implementação de atributos da classe PonstosColeta - Luna

--- ApiRestReGraphik/Models/PontosColeta.cs
+++ ApiRestReGraphik/Models/PontosColeta.cs
@@ -1,6 +1,24 @@
-﻿namespace ApiRestReGraphik.Models
+﻿using System.Text.Json.Serialization;
+
+namespace ApiRestReGraphik.Models
 {
     public class PontosColeta
     {
+        [JsonPropertyName("id")]
+        public int Id { get; set; }
+        [JsonPropertyName("nome_ponto")]
+        public string NomePonto { get; set; }
+
+        [JsonPropertyName("cidade")]
+        public string Cidade { get; set; }
+
+        [JsonPropertyName("estado")]
+        public string Estado { get; set; }
+
+        [JsonPropertyName("cep")]
+        public string CEP { get; set; } 
+
+        [JsonPropertyName("residuos_aceitos")]
+        public string ResiduosAceitos { get; set; } 
     }
 }
