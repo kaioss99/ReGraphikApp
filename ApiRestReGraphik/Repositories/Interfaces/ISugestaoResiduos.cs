f2e1f6bd671267fdcfc6ac2d793f0552dea82545
Luna <luna.beatrizalves@gmail.com>
2026-05-24 15:56:55 -0300

    Criando interface de sugestão de residuos - Luna

---
+++ ApiRestReGraphik/Repositories/Interfaces/ISugestaoResiduos.cs
@@ -0,0 +1,17 @@
+﻿using ApiRestReGraphik.Models;
+
+namespace ApiRestReGraphik.Repositories.Interface
+{
+    /// <summary>
+    /// Define os métodos para acessar e manipular os dados relacionados às sugestões de resíduos, 
+    /// como listar, obter por ID, adicionar, atualizar e excluir sugestões de resíduos.
+    /// </summary>
+    public interface ISugestaoResiduos
+    {
+        Task<List<SugestaoResiduo>> GetAll();
+        Task<SugestaoResiduo> GetById(string id);
+        Task Add(SugestaoResiduo sugestaoResiduo);
+        Task Update(string id, SugestaoResiduo sugestaoResiduo);
+        Task Delete(string id);
+    }
+}
